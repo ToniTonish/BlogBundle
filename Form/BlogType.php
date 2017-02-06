cf2bf60a66108a2852d14cbb5828188f4f03b4ca
Antonino Buscetta <antonino.buscetta@gmail.com>
2017-02-06 08:56:54 +0100

    add new layout in blog creation

--- Form/BlogType.php
+++ Form/BlogType.php
@@ -13,7 +13,10 @@ class BlogType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('content')        ;
+        $builder
+            ->add('title')
+            ->add('content')
+        ;
     }
     
     /**
